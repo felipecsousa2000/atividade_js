09a89fa7cbc571bb392e00a5b0ebb5ca2884ee61
felipecsousa2000 <132520533+felipecsousa2000@users.noreply.github.com>
2026-05-13 02:45:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+// script.js
+
+function corrigir1(){
+
+    let resposta = document.getElementById("p1").value;
+
+    if(resposta == "alert"){
+        document.getElementById("r1").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r1").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir2(){
+
+    let resposta = document.getElementById("p2").value;
+
+    if(resposta == "console.log"){
+        document.getElementById("r2").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r2").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir3(){
+
+    let resposta = document.getElementById("p3").value;
+
+    if(resposta == "let"){
+        document.getElementById("r3").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r3").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir4(){
+
+    let resposta = document.getElementById("p4").value;
+
+    if(resposta == "document.getElementById"){
+        document.getElementById("r4").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r4").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir5(){
+
+    let resposta = document.getElementById("p5").value;
+
+    if(resposta == "innerText"){
+        document.getElementById("r5").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r5").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir6(){
+
+    let resposta = document.getElementById("p6").value;
+
+    if(resposta == "onclick"){
+        document.getElementById("r6").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r6").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir7(){
+
+    let resposta = document.getElementById("p7").value;
+
+    if(resposta == "value"){
+        document.getElementById("r7").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r7").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir8(){
+
+    let resposta = document.getElementById("p8").value;
+
+    if(resposta == "function"){
+        document.getElementById("r8").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r8").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir9(){
+
+    let resposta = document.getElementById("p9").value;
+
+    if(resposta == "document"){
+        document.getElementById("r9").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r9").innerText =
+        "❌ Errado!";
+    }
+
+}
+
+function corrigir10(){
+
+    let resposta = document.getElementById("p10").value;
+
+    if(resposta == "alert"){
+        document.getElementById("r10").innerText =
+        "✅ Correto!";
+    }
+
+    else{
+        document.getElementById("r10").innerText =
+        "❌ Errado!";
+    }
+
+}
